382474cee4e66f7765472327f1feca729b4deef0
dj.jeong <sam1087@mju.ac.kr>
2024-12-05 00:12:32 +0900

    [60191685 정동준] api 스펙 추가

--- api-server/main.py
+++ api-server/main.py
@@ -1,24 +1,30 @@
-import datetime
-import json
-import os
-from dotenv import load_dotenv
+from typing import Union
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def read_root():
+    return {"Hello": "World"}
 
-@app.get("/search")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/search/{yearmonth}")
+async def search(yearmonth: int):
+    """
+    <div>
+    <div>매매가 변동 원인을 나타내는 기사를 조회하는 API</div>
+    <br />
+    <div><strong>Path Parameters</strong></div>
+    <div>----------</div>
+    <div>yearmonth(str):&nbsp;조회하고자 하는 매매가 변동 기간</div>
+    <br />
+    <div><strong>Returns</strong></div>
+    <div>-------</div>
+    <div>articles(json list): 원인을 나타내는 기사들을 json 리스트로 반환</div>
+    </div>
+    """
+
+    articles = []
+    return { "articles": articles }
